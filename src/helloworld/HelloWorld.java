2c6934d69ce60e50835218e9b78094f3f331e810
z1210 <z1210@DESKTOP-B40TBOQ>
2019-05-23 21:30:10 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,8 +1,7 @@
 package helloworld;
  
 public class HelloWorld {
- 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World  达到sad");
     }
 }
